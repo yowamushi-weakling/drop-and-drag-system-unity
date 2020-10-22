493f9de89adc43ceb68a7b2f3977c14ead57a0eb
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-10-22 20:51:36 +0700

    Add empty project

---
+++ Drag and Drop Puzzle/Drag and Drop Puzzle/Assets/Script/WinScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    private int poinsToWin;
+    private int currentPoints;
+    public GameObject myIdol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poinsToWin = myIdol.transform.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPoints >= poinsToWin)
+        {
+            // WIN
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void AddPoints()
+    {
+        currentPoints++;
+    }
+}
